7a17e09edc757a23cbe621ae2916519ce021ee90
haraldsDev <Haralds.Dev@gmail.com>
2018-11-07 15:36:27 +0200

    started redux Store and Provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
+import { searchRobots } from './reducers';
+
+const store = createStore(searchRobots);
 
 ReactDOM.render(
-		<App />
-		, document.getElementById('root'));
+		<App store={store}/>, document.getElementById('root'));
 registerServiceWorker();
 
 
--- src/reducers.js
+++ src/reducers.js
@@ -1,12 +1,12 @@
 import { CHANGE_SEARCH_FIELD } from './constants.js';
 
-const initialState {
+const initialState = {
 	searchField: '',
 }
 
 export const searchRobots = (state=initialState, action={}) => {
 	switch(action.type) {
-		case: CHANGE_SEARCH_FIELD:
+		case CHANGE_SEARCH_FIELD:
 			return Object.assign({}, state, {searchField: action.payload});
 		default:
 			return state;
